2a507580fed28dfb0175a9ed3f6310f2bd0e4563
alifhanafiah <muhalifhanafiah@gmail.com>
2023-12-06 12:38:12 +0800

    Adding the Theme HTML

--- app/Models/Listings.php
+++ app/Models/Listings.php
@@ -2,32 +2,10 @@
 
 namespace App\Models;
 
-class Listings
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listings extends Model
 {
-  public static function all()
-  {
-    return [
-      [
-        'id' => 1,
-        'title' => 'Listing One',
-        'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus asperiores qui a temporibus amet, voluptas voluptatem consectetur. Ipsum, sapiente delectus.'
-      ],
-      [
-        'id' => 2,
-        'title' => 'Listing Two',
-        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, minus?'
-      ]
-    ];
-  }
-
-  public static function find($id)
-  {
-    $listings = self::all();
-
-    foreach ($listings as $listing) {
-      if ($listing['id'] == $id) {
-        return $listing;
-      }
-    }
-  }
+    use HasFactory;
 }
